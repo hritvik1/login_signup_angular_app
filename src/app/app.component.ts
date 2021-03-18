5ea8c38730e56e1b73cd3787f7239a40c6506296
Hritvik Maini <hritvikmaini00@gmail.com>
2021-03-19 03:35:24 +0530

    Updated view_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'login-signup-app';
+  constructor(private router: Router) {}
+  login = () => {
+    this.router.navigateByUrl('/login');
+  }
+  signup = () => {
+    this.router.navigateByUrl('/signup');
+  }
 }
